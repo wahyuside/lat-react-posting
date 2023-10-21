08d3ef4e5b809a1cc3ab2efeb78fc5f64c6f5ccb
Wahyu Said <boxout.wsn@gmail.com>
2023-10-22 02:11:34 +0700

    completing users on server

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,15 +1,30 @@
-const { User } = require("../models/users");
+const { users } = require("../models");
 
 class UserController {
   static async getUsers(req, res) {
     try {
-      const result = await User.findAll();
+      let result = await users.findAll();
+      console.log(result);
       res.status(200).json(result);
     } catch (error) {
-      res.status(500).json(error);
+      res.status(500).json("Error " + error);
     }
   }
-  //   static async register() {}
+  static async register(req, res) {
+    try {
+      const { username, email, password, img, role } = req.body;
+      let result = await users.create({
+        username,
+        email,
+        password,
+        img,
+        role,
+      });
+      res.status(201).json(result);
+    } catch (error) {
+      res.status(500).json("Error " + error);
+    }
+  }
 }
 
 module.exports = UserController;
--- server/routes/user.js
+++ server/routes/user.js
@@ -2,5 +2,6 @@ const userRoute = require("express").Router();
 const UserController = require("../controllers/UserController");
 
 userRoute.get("/", UserController.getUsers);
+userRoute.post("/register", UserController.register);
 
 module.exports = userRoute;
